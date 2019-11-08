853091432b79870332b7a727e8b39df09c27e747
Danurai <Danurai@users.noreply.github.com>
2019-11-08 17:22:58 +0000

    Delete fellowship-core.cljs

--- src/cljs-lotrfellowship/danuraidb/fellowship-core.cljs
+++
@@ -1,6 +0,0 @@
-(ns danuraidb.fellowship-core
-  (:require
-    [reagent.core :as r]
-    [danuraidb.fellowship-view :as view]))
-    
-(r/render [view/page] (.getElementById js/document "fellowship"))
