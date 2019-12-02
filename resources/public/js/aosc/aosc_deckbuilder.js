c15fa7e21a4387448e31ee59e45566e9e88e3be9
Danurai <Danurai@users.noreply.github.com>
2019-12-02 11:03:50 +0000

    Add files via upload

---
+++ resources/public/js/aosc/aosc_deckbuilder.js
@@ -0,0 +1,391 @@
+var _cards;
+var _collection;
+var wavecounts = {};
+var _filter = {};
+var _filter_custom = {};
+var _sort = "id asec";
+var _decklist = [];
+var _owned = false;
+var _lang = "en";
+
+var converter = new showdown.Converter();
+
+
+$.getJSON('/aosc/api/data/cards',function (data) {
+  var source_data = data['hits']['hits'].map(c => c['_source']);
+  _cards = TAFFY(
+    source_data.map(card => $.extend({"setnumber": card.set[0].number,"setname": card.set[0].name,"text": card.effect.en, },card))
+  );
+  
+  $.getJSON('/aosc/api/private/collection', function(data) {
+    _collection = data;
+    $.each(_collection, function (k, v) {
+      _collection[k].total = (typeof v.digital !== 'undefined' ? parseInt(v.digital,10) : 0) + (typeof v.physical !== 'undefined' ? parseInt(v.physical,10) : 0)
+    });
+    
+    try {
+      _decklist = parse($('#deckdata').val()).cards;
+      $.each(_decklist, function (id, item) {
+        update_alliance(item.id);
+      });
+    } catch (e) {console.log (e)}
+    _filter.alliance = [$('#filter_alliance').find("input[type=radio]:checked").prop("id"),"Any"];
+    
+    write_table();
+    write_deck();
+    $('#usernotes').trigger('input');
+  });
+});
+
+function write_table () {
+  var crd;
+  $('#tblbody').empty();
+  _cards(_filter).filter(_filter_custom).order(_sort).each(function (c)  {
+    crd = $.extend({"digital":0,"physical":0,"foil":0},_collection[c.id],c);
+    crd.owned = parseInt(crd.digital,10) + parseInt(crd.physical,10) + parseInt(crd.foil,10);
+    $('#tblbody').append(table_row (crd));
+  });
+}
+function table_row (c)  {
+  var hiderow = $('#lock').prop('checked');
+  return '<tr class="'
+       + getCardClass(c)
+       + (c.owned == 0 ? ' table-secondary text-muted' + (hiderow ? ' hiderow' : '') : '') 
+      + '">'
+    + '<td>' + card_button_group(c) + '</td>'
+    + '<td><span data-toggle="modal" data-target="#cardmodal" data-id="'+  c.id + '">'
+      + '<a href="#" data-id = "' + c.id + '" class="card-tooltip" data-toggle="popover">'
+      + ($.inArray("Unique",c.tags) != -1 ? '&bull;&nbsp;' : '')
+      + c.name + '</span></span></td>'
+    + '<td><img class="trait-icon" src="/img/aosc/icons/category_' + c.category.en.toLowerCase() + '.png"></td>'
+    + '<td class="text-center">' 
+      + trait_icons(c.tags, true)
+      + '</td>'
+    + '<td class="text-center">' + (typeof c.healthMod !== 'undefined' ? '<span class="hp">' + c.healthMod + '</span>': '') + '</td>'
+    + '<td class="text-center">' + (typeof c.cost !== 'undefined' ? '<span class="cost">' +  c.cost + '</span>': '') + '</td>';
+}   
+function card_button_group(c) {
+  var outp = '<div class="btn-group btn-group-toggle btn-group-xs" data-toggle="buttons">';
+  var deckdata = _decklist.filter(function(c) {return c.id == this}, c.id);
+  var count = (deckdata.length > 0 ? deckdata[0].count : 0);
+  var max = maxAllowedInDeck(c);
+  for (var x=0; x<=Math.min(3, max); x++) { //limit to 3+ for table layout
+    outp += '<label class="btn '
+      + (c.owned < x ? "btn-outline-danger" : "btn-outline-secondary")
+      + (x == count ? ' active' : '') + '">'
+      + '<input type="radio" name="' + c.id + '" val=' + x + ' data-id = ' + c.id + '>'
+      + x 
+      + '</label>';
+  }
+  if (max > 3) {outp += '<label class="btn '
+      + (c.owned < 4 ? "btn-outline-danger" : "btn-outline-secondary") + (count > 3 ? ' active' : '') + '"'
+      + 'data-id = ' + c.id + ' data-toggle="modal" data-target="#cardmodal">'
+      + '<input type="radio" name="' + c.id + '" val=4>'
+      + '+'
+      + '</label>';
+  }
+  outp += '</div>';
+  return outp;
+}
+function maxAllowedInDeck(c) {
+  if (c._id == -117917574 || c.name == "Festering Nurglings"){   //Festering Nurglings
+    return 7;
+  } else if ($.inArray("Unique", c.tags) == true || c.category.en == "Blessing") {
+    return 1;
+  } else if (c.category.en == "Champion") {
+    return 2;
+  } else {
+    return 3;
+  }
+}
+
+function write_deck() {
+  var deck = TAFFY();
+  var card;
+  var generated_deck = (_decklist.length != 0 ? generate(_decklist) : "")
+  $('#deckdata').val(generated_deck);
+  
+  write_deck_cards(
+      TAFFY(
+      _decklist.map(
+        function (c) {
+           return $.extend(c,this({"id":c.id}).first())
+        }, _cards)));
+}
+
+function write_deck_cards (deck)  {
+  var card_types = ["Champion","Blessing","Unit","Spell","Ability"];
+  var outp = '';
+  var cost = 0;
+  var health = 30;
+  deck().each(function (c) {
+    cost += (typeof c.cost !== 'undefined' ? c.cost * c.count : 0)
+    health += (typeof c.healthMod !== 'undefined' ? c.healthMod * c.count : 0)
+  });
+// Header
+  var actions = deck({"category":{"en":["Unit","Ability","Spell"]}}).sum('count');
+  var uniqueChampions = deck({"category":{"en":"Champion"},"tags":{"has":"Unique"}}).count();
+  outp += '<div class="row-fluid my-2">'
+    + (uniqueChampions > 1 ? '<span class="text-danger mr-1">More than one Unique Champion!</span>' : '')
+    + 'Cards: <span class="' + (actions != 30 ? 'text-danger' : 'text-success') + '">' + actions + '/30  </span>'
+    + 'Cost: <span class="' + (cost > 20 ? 'text-danger' : 'text-success') + '">' + cost + "/20  </span>"
+    + 'Health: ' + (Math.min (health, 35))
+    + '</div>';
+// Body
+  outp += '<div class="row mb-1"><div class="col-lg-6">'
+    + write_section(deck, "Champion")
+    + '</div><div class="col-lg-6">'
+    + write_section(deck,"Blessing")
+    + '</div></div>';
+  outp += '<div class="row"><div class="col-lg-6">'
+    + (deck({"category":{"en":"Unit"}}).count() > 0 ? write_section(deck, "Unit") : '')
+    + '</div><div class="col-lg-6">'
+    + (deck({"category":{"en":"Spell"}}).count() > 0 ? write_section(deck, "Spell") : '')
+    + (deck({"category":{"en":"Ability"}}).count() > 0 ? write_section(deck, "Ability") : '')
+    + '</div></div>';
+  $('#decklist').html(outp);
+}
+function write_section (deck, type) {
+  var outp = '';
+  var card_cls = '';
+  outp += '<b>' + type + '</b> (' + deck({"category":{"en":type}}).sum('count') + ')';
+  deck({"category":{"en":type}}).order("name asec").each(function (c) {
+    card_cls = getCardClass(c)
+    
+    outp += '<div class="' + card_cls + '">'      
+      + '<span data-toggle="modal" data-target="#cardmodal" data-id="'+  c.id + '">'
+      + '<a href="/card/' + c.id + '" data-id = "' + c.id + '" class="card-tooltip" data-toggle="popover">'
+      + c.count + 'x ' 
+      + c.name 
+      + ($.inArray("Unique",c.tags) != -1 ? '&nbsp;(U)' : '')
+      + '</a></span>'
+      + trait_icons(c.tags, false)
+      + '</div>';
+  });
+  return outp;
+}
+function getCardClass (c) {
+  if (typeof c.class !== "undefined") {
+    switch (c.class["en"]) {
+      case "Warrior":
+      case "Wizard":
+      case "Any":
+        return "class-" + c.class["en"].toLowerCase();
+        break;
+      case "Warrior Wizard":
+        return "class-warrior-wizard";
+        break;
+      default:
+        return "class-any";
+    }
+  } else {
+    switch (c.category["en"]) {
+      case "Unit":
+        return "class-warrior";
+        break;
+      case "Spell":
+        return "class-wizard";
+        break;
+      default:
+        return "class-any";
+    }
+  }
+}
+
+function trait_icons (traits) {
+  var outp = '';
+  if (typeof traits !== 'undefined')  {
+    $.each(traits, function (id, trait) {
+      outp += '<img class="img-fluid trait-icon ml-1" src="/img/aosc/icons/tag_' + trait.toLowerCase() + '.png"></img>';
+    });
+  }
+  return outp;
+}
+
+function trait_icons_icomoon (traits)  {
+  var outp = '';
+  if (typeof traits !== 'undefined')  {
+    $.each(traits.filter(t => t != "Unique"), function (id, trait) {
+      outp += '<span class="type-icon ml-1"><span class="aosc-type-' + trait.toLowerCase() + '" title="' + trait + '"></span></span>'; 
+    });
+  }
+  return outp;
+}
+
+$('#filter_type').on('change',function() {
+  var types = [];
+  delete _filter.category;
+  $(this).closest('div').find('input:checked').each(function (id, btn) {
+    types.push (btn.id);
+  });
+  if (types.length > 0) {_filter.category = {}, _filter.category[_lang] = types}
+  write_table();
+});
+
+$('#filter_alliance').on('change','input[type=radio]',function() {
+  _filter.alliance = [this.id,"Any"];
+  write_table();
+});
+$('#selecttrait').on('changed.bs.select',function() {
+  delete _filter.tags;
+  if ($(this).val().length > 0) {
+    _filter.tags = {"has": $(this).val()};
+  }
+  write_table();
+});
+
+// sort
+$('thead').on('click','.sortable',function() {    
+  var field = $(this).data("field");
+  switch(_sort)  {
+    case field + ' asec': _sort = field + ' desc'; break;
+    case field + ' desc': _sort = ''; break;
+    default: _sort = field + ' asec';
+  }
+  $.each(this.parentNode.getElementsByClassName("caret"), function (id, node) {
+    node.remove();
+  });
+  if (_sort != "") {
+    $(this).append('<span class="caret"><i class="fas fa-caret-' + (_sort.slice(-4) == "asec" ? 'down' : 'up') + '"></span>');
+  }
+  write_table();
+});
+
+$('#lock').on('change',function () {
+  $(this).parent()
+    .find('[data-fa-i2svg]')
+    .toggleClass("fa-lock")
+    .toggleClass("fa-unlock");
+  $('#tblbody').find('tr.table-secondary').toggleClass("hiderow");
+});
+
+$('body')
+  .on('click','.cardlink',function (ev) {
+    ev.preventDefault();
+  })
+  .on('mouseover','.card-tooltip',function () {
+    setpopover(this, _cards({"id":$(this).data("id")}).first());
+  })
+  .on('click','.card-tooltip',function (ev) {
+    ev.preventDefault();
+  });
+
+$('#tblbody').on('change','input[type=radio]',function() {
+  if ($(this).attr("val") < 4) { // only trigger for 0-3
+    updateDeckCount($(this).data("id"), $(this).attr("val"));
+  } else {
+    write_table();
+  }
+});
+
+
+$('#cardmodal')
+  .on('show.bs.modal', function (event) {
+    var id = $(event.relatedTarget).data("id"); // Button that triggered the modal
+    var crd = _cards({"id":id}).first();
+    if (typeof crd.id !== 'undefined') {setModalHtml($(this),crd);}
+  })
+  .on('hidden.bs.modal', function () {
+    $(this).find('.modal-header').html('');
+    $(this).find('.modal-body').html('');
+  })
+  .on('change','input[type=radio]',function () {
+    updateDeckCount($(this).data("id"), $(this).attr("value"));
+    $('#cardmodal').modal('hide');
+  })
+  .on('keypress',function (ev)  {
+    var num = parseInt(ev.which, 10) - 48;
+    $('.modal input[type=radio][value=' + num + ']').trigger('change');
+  });;
+
+function setModalHtml(modal,crd) {
+  modal
+    .find('.modal-header')
+    .html('<div class="modal-title"><h4 class="mr-2 align-middle">' 
+      + ($.inArray("Unique",crd.tags) != -1 ? '&bull;&nbsp;' : '')
+      + crd.name 
+      + '</h4>' + modalButtonGroup(crd)
+      + '</div>'
+      + '<button class="close" type="button" data-dismiss="modal"><span>&times;</span></button>');
+  modal
+    .find('.modal-body')
+    .html('<div class="row">'
+      + '<div class="col-sm-9">'
+      + '<img src="/img/aosc/cards/' + crd.skus.filter(sku => (sku.default == true && sku.lang == "en"))[0].id + '.jpg" style="width: 100%;"></img>'
+      + '</div></div>');
+}  
+function modalButtonGroup(crd) {
+  crd = $.extend({"digital":0,"physical":0,"foil":0},_collection[crd.id],crd);
+  crd.owned = parseInt(crd.digital,10) + parseInt(crd.physical,10) + parseInt(crd.foil,10);
+  var deckdata = _decklist.filter(function(c) {return c.id == this}, crd.id);
+  var count = (deckdata.length > 0 ? deckdata[0].count : 0);
+  var outp = '<div>';
+  outp += '<div class="btn-group btn-group-toggle" data-toggle="buttons">';
+  for (i=0; i<=maxAllowedInDeck(crd); i++) {
+    outp += '<label class="btn'
+      + (crd.owned < i ? ' btn-outline-danger' : ' btn-outline-secondary')
+      + (count == i ? ' active' : '')
+      + '">'
+      + '<input type="radio" value="' + i + '" name="' + crd.id + '" data-id="' + crd.id + '">'
+      + i
+      + '</label>';
+  }
+  outp += '</div></div>';
+  return outp;
+}
+
+function updateDeckCount (id, count) {
+  _decklist = _decklist.filter(function (c) {return c.id != this}, id);
+  if (count > 0) {
+    _decklist.push({"id": id, "count": parseInt(count)})
+  }
+  update_alliance(id);
+  write_table();
+  write_deck();
+}
+
+function update_alliance(id)  {
+  var c = _cards({"id":id}).first();
+  if (c.alliance != "Any") {$('#deckalliance').val(c.alliance)}
+}
+
+// SEARCH
+$('#filtertext').on('input', function () {
+  _filter_custom = parsefilter($(this).val());
+  write_table()
+});
+  
+// typeahead 
+$('#filtertext')
+  .typeahead({
+    hint: true,
+    highlight: true,
+    minLength: 2
+  },
+  {
+    name: 'cardnames',
+    source: function findMatches(q,cb) {
+              cb(_cards({"name":{"likenocase":q}}).select("name"));}
+  })
+  .on('typeahead:select typeahead:autocomplete', function(ev, suggestion) {
+    var trigger = $(this);
+    $('#cardmodal').on('hidden.bs.modal',function() {
+      setTimeout(function () {
+        trigger.typeahead('val','').trigger('input').focus();
+      },100);
+    });
+    var card = _cards({"name":suggestion}).first();
+    setModalHtml($('#cardmodal'),card)
+    $(this).data("id", card.id);
+    $('#cardmodal').modal('show');
+  }); 
+
+
+// DECK NOTES TAB
+$('#usernotes').on('input',function () {
+  $('#notesmarkdown').html( converter.makeHtml($(this).val()) );
+  $('#decknotes').val( $(this).val() );
+});
+
+
+// CHECK Tab
---
+++ resources/public/js/aosc/aosc_quickcollection.js
@@ -0,0 +1,284 @@
+var _filter = {"setnumber":2.1};
+var _freeFilter = {};
+var _cards;
+var _collection;
+var _localpath = "/img/aosc/cards/";
+var _remotepath = "https://assets.warhammerchampions.com/card-database/cards/";
+
+
+var cval = $('#collection').val();
+_collection = (cval != "" ? JSON.parse(cval) : {});
+
+$.getJSON('/aosc/api/data/cards', function(data) {
+  var data_source = data['hits']['hits'].map(c => c['_source']);
+  _cards = TAFFY (
+    data_source
+      .map(function(c) {return $.extend({"digital":0,"physical":0,"foil":0},this[c.id],c)}, _collection)
+      .map(function(c) {
+        return $.extend({
+          "catid":this.indexOf(c.category.en),
+          "setnumber":c.set[0].number,
+          "text":c.effect.en,
+          "maxqty":getMaxQty($.inArray("Unique",c.tags)>-1, c.category.en),
+          "craftcost":getCraftCost(c.category.en, c.rarity[0])
+        },c)},["Champion","Blessing","Unit","Spell","Ability"])
+  )
+  _filter.alliance =  $('#alliance').find('input:checked').first().attr('id');
+  _filter.category =  {"en":$('#category').find('input:checked').first().attr('id')};
+  
+  write_cards();
+});
+
+function getMaxQty (unique, cat) {
+  return (unique ? 1 
+    : (cat == "Champion" ? 2
+        : (cat == "Blessing" ? 1
+            : 3)))
+}
+
+function getCraftCost (cat, rarity) {
+  return (cat == "Blessing" && rarity == "E" 
+    ? 2500
+    : (cat == "Champion" || cat == "Blessing"
+      ? {"C": 150, "U": 375, "R": 750, "E": 1000}[rarity]
+      : {"C": 100, "U": 250, "R": 500, "E": 750}[rarity] ))
+}
+
+function write_stats () {
+   var outp = '';
+   $.each(_cards().order("setnumber").distinct("setnumber"),function (k, v) {
+     outp += '<span class="mr-2">' 
+       + '<b>' + _cards({"setnumber":v}).first().set[0].name + ": </b>" 
+       + _cards({"setnumber":v,"digital":{">":0}}).count()
+       + "/" + _cards({"setnumber":v}).count()
+       + '</span>';
+   });
+   $('#stats').html(outp);
+}
+
+
+function imageName(c) {
+  var sku = c.skus.filter(sku => (sku.default == true && sku.lang == "en"))[0]
+  return sku.id + ".jpg"
+}
+
+
+var tableimg = new Image();
+tableimg.onload = function () {
+    write_table(true);
+  }
+tableimg.onerror = function () {
+    write_table(false);
+  }
+  
+function write_cards() {
+  tableimg.src = _remotepath + imageName(_cards().first());
+}
+//  $.get(_localpath + imageName(_cards().first()),function () { 
+//    write_table(_localpath);
+//  })
+//  .fail(function () {
+//    write_table(_remotepath);
+//  });
+//}
+
+function write_table(remotepath) {
+  var outp = '<table class="mx-auto"><tbody>';
+  var c;
+  var total = 0;
+  var res = _cards($.extend({},_filter, _freeFilter)).order("catid asec, name asec").get();
+  
+  if ($('#incomplete').find('input:checked').length > 0) {
+    res = res.filter(c=>c.digital+c.physical+c.foil<c.maxqty);
+  }
+  outp += '<tr>'
+  for (i=0; i<res.length; i++) {
+    if (((i % 7) == 0) && (i != 0)) {
+       outp += '</tr><tr>';
+    }
+    total = (parseInt(res[i].digital) + parseInt(res[i].physical) + parseInt(res[i].foil));
+    outp += '<td><div class="cardcontainer d-flex" data-id=' + res[i].id + '>'
+      + (remotepath
+        ? '<img class="cardimg' + (total == 0 ? ' cardimggrey' : '') + '" src="' + imgpath + imageName(res[i]) + '" alt="' + res[i].name + '">'
+        : '<div class="cardimg' + (total == 0 ? ' cardimggrey' : '') + ' placeholder placeholder-' + res[i].alliance.toLowerCase() + '">'
+          + '<span class="m-auto"> ' + res[i].name + '</span>'
+          + '</div>') 
+      + '<span class="collectionbox ' + (total == 0 ? 'lockbox' : 'countbox') + '">'
+        + '<span data-id=' + res[i].id + ' data-toggle="modal" data-target="#updatemodal">'
+          + (total == 0 ? '<i class="fa fa-lock">' : 'x' + total)
+        + '</span></span>'
+      + '</div></td>';
+  }
+  outp+='</tr>';
+  $('#cards').html (outp + '</tbody></table>');
+  write_stats();
+}
+
+function valueUpdateInputs(crd) {
+  return '<div class="row" data-id = ' + crd.id + '>'
+      + '<div class="col-sm-4">'
+        + '<div class="input-group">'
+          + '<div class="input-group-prepend">'
+          + '<span class="input-group-text" title="Digital"><i class="fas fa-mobile-alt" /></span>'
+          + '</div>'
+          + '<input type="number" data-type="digital" class="form-control" value=' + crd.digital + '></input>'
+        + '</div>'
+      + '</div>'
+      + '<div class="col-sm-4">'
+        + '<div class="input-group">'
+          + '<div class="input-group-prepend">'
+          + '<span class="input-group-text" title="Physical"><i class="fas fa-globe" /></span>'
+          + '</div>'
+        + '<input type="number" data-type="physical" class="form-control" value=' + crd.physical + '></input>'
+        + '</div>'
+      + '</div>'
+      + '<div class="col-sm-4">'
+        + '<div class="input-group">'
+          + '<div class="input-group-prepend">'
+          + '<span class="input-group-text" title="Foil"><i class="far fa-gem" /></span>'
+          + '</div>'
+        + '<input type="number" data-type="foil" class="form-control" value=' + crd.foil + '></input>'
+        + '</div>'
+      + '</div>'
+    + '</div>';
+}
+function valueUpdateInputsX(crd) {
+  return '<div class="row" data-id = ' + crd.id + '>'
+      + '<div class="col-sm-4">'
+      + '<label>Digital</label>'
+      + '<input type="number" data-type="digital" class="form-control" value=' + crd.digital + '></input>'
+      + '</div>'
+      + '<div class="col-sm-4">'
+      + '<label>Physical</label>'
+      + '<input type="number" data-type="physical" class="form-control" value=' + crd.physical + '></input>'
+      + '</div>'
+      + '<div class="col-sm-4">'
+      + '<label>Foil</label>'
+      + '<input type="number" data-type="foil" class="form-control" value=' + crd.foil + '></input>'
+      + '</div>'
+    + '</div>';
+}
+
+$('#updatemodal').on('show.bs.modal', function (evt) {
+  var id = $(evt.relatedTarget).closest('div').data('id');
+  var crd = _cards({"id":id}).first();
+  var $modalBody = $(this).find('.modal-body');
+  $.get(_localpath + imageName(crd),function () { 
+      setModalBody($modalBody, _localpath, crd);
+    })
+    .fail(function () {
+      setModalBody($modalBody, _remotepath, crd);
+    });
+  $(this).find('.modal-title').html('<h4>' + crd.name + '<span class="ml-2" style="font-size: 0.8rem;"> Craft: ' + crd.craftcost + '</span></h4>');
+  $($(this).find('.modal-footer a')[0]).attr("href","/aosc/cards/" + crd.id);
+});
+
+function setModalBody ( $modalBody, path, crd ) {
+  $modalBody.html(
+    valueUpdateInputs (crd) 
+     + '<img class="mt-2" src="' + path + imageName(crd) + '" onerror="imgerr"></img>');
+}
+
+$('#updatemodal').on('change','input[type=number]',function () {
+  var id = $(this).closest('.row').data('id');
+  var type = $(this).data("type");
+  var val = parseInt($(this).val());
+  
+  update_card_collection (id, type, val);
+});
+
+// Double-click to quick add 1x Digital
+
+$(document).on('dblclick','.cardcontainer', function() {
+  var id = $(this).data('id');
+  var type = 'digital';
+  var val = 1 + parseInt(typeof _collection[id] === 'undefined' ? 0 : _collection[id]['digital'])
+  update_card_collection (id, type, val);
+});
+
+function update_card_collection (id, type, val) {
+  if (typeof _collection[id] === 'undefined') {_collection[id] = {};}
+  _collection[id][type] = val;
+  _cards({"id":id}).update(type,val);
+  var crd = _cards({"id":id}).first();
+  var total = "x" + (parseInt(crd.digital) + parseInt(crd.physical) + parseInt(crd.foil));
+  $('#collection').val(JSON.stringify(_collection));
+  write_stats();
+  
+  var card_container = $('#cards').find('.cardcontainer[data-id=' + id + ']');
+  var card_collection = card_container.find('.collectionbox');
+  if (total == "x0") {
+    card_container.find('img').addClass('cardimggrey');
+	card_collection.removeClass("countbox").addClass("lockbox");
+    card_container.find('.collectionbox').find('span').html('<i class="fa fa-lock"></i>');
+  } else {
+	card_collection.removeClass("lockbox").addClass("countbox");
+    card_container.find('img').removeClass('cardimggrey');
+    card_container.find('span.countbox').find('span[data-id=' + id + ']').html(total);
+  }
+  $('#btnsave').prop("disabled",false);
+}
+  
+$('#incomplete').on('click', function () {
+    $(this).find('label').blur();
+    write_cards();
+    });
+  
+// FILTERS
+$('.btn-group-toggle-none label.btn').on('click',function() {
+  
+  var toggleon = $(this).find('input:checked').length == 0;
+  var f = $(this).closest('div')[0].id;
+  
+  var grp = $(this).closest('div')[0];
+  $(grp).find('label').removeClass('active');
+  $(grp).find('input').prop('checked',false);
+  
+  delete _filter[f]
+  if (toggleon) {
+    $(this).addClass('active');
+    $(this).find('input').prop('checked',true);
+    var v = $(this).find('input')[0].id;
+    if (f=="category") { _filter[f] = {"en":v} }
+    else { _filter[f] = v; }
+  }
+  
+  write_cards();
+  return false; // don't fire twice!
+});
+
+$('#filter').on('input',function() {
+    _freeFilter = parsefilter($(this).val());
+    write_cards();
+  });
+
+// IMPORT //
+$('#inputcollection').on('input',function() {
+  $('#importcollection').val($(this).val())
+});
+
+$('#copycollection').on('click',function() {
+  $temp=$('<input>');
+  $('body').append($temp);
+  $temp.val($('#collection').val()).select();
+  document.execCommand("copy");
+  $temp.remove();
+  add_toast("Collection copied to clipboard");
+  $(this).select();
+});
+
+
+var staging_uri = "https://danuraidb.herokuapp.com/staging"
+
+$('#stagecollection').on('click', function () {
+  var dx = {
+    name: "Collection",
+    type: "collection",
+    system: 1,
+    decklist: $('#collection').val()
+  }
+  $.post(staging_uri,dx,function(x) {
+    console.log(x);
+    add_toast("Collection Staged.");
+  });
+});
