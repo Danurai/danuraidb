c02c5d1da80c67c3a22622a2949c80660709898b
Danurai <Danurai@users.noreply.github.com>
2021-01-13 17:09:48 +0000

    Add files via upload

--- resources/public/js/lotrdb/lotrdb_popover.js
+++ resources/public/js/lotrdb/lotrdb_popover.js
@@ -1,4 +1,5 @@
 var _cards;
+var _cardsdigital;
 var _packs;
 const icon_unique = '<i class="lotr-type-unique unique-icon mr-1"></i>'; 
 
@@ -10,29 +11,36 @@ function lortdb_markdown (str) {
   rtn = str.replace(/\[(\w+)\]/g,(($0,$1) => '<i class="lotr-type-' + $1.toLowerCase() + '"></i>'));
   return rtn;
 }
+// resource
+// threat
 
+  
+function card_icon(c) {
+  var img = '';
+  img = '<img class="icon-sm float-right" src="/img/lotrdb/icons/'
+  switch (c.type_code) {
+    case 'hero':
+    case 'attachment':
+    case 'ally':
+    case 'event':
+      img += 'sphere_' + c.sphere_code;
+      break;
+    default:
+      img += 'pack_' + c.pack_code;
+  }
+  img += '.png"></img>'
+  return img
+}
+
+
+$.getJSON("/lotrdb/api/data/cardsdigital", function (data) {
+  _cardsdigital = data;
+});
 
 $.getJSON("/lotrdb/api/data/cards", function (data) {
   _cards = TAFFY(data);
   $.getJSON("/lotrdb/api/data/packs", function (data) {
     _packs = TAFFY(data)
-  
-    function card_icon(c) {
-      var img = '';
-      img = '<img class="icon-sm float-right" src="/img/lotrdb/icons/'
-      switch (c.type_code) {
-        case 'hero':
-        case 'attachment':
-        case 'ally':
-        case 'event':
-          img += 'sphere_' + c.sphere_code;
-          break;
-        default:
-          img += 'pack_' + c.pack_code;
-      }
-      img += '.png"></img>'
-      return img
-    }
     
     $('body').on('mouseover','.card-link',function () {
       crd = _cards({"code":$(this).data('code').toString()}).first();
@@ -87,3 +95,37 @@ $('.search-info').popover({
     + '<br>d: deck (<b>p</b>layer <b>e</b>nemy <b>q</b>uest)'
     + '<br>u:true|false Unique'
 });
+
+
+  $('body').on('mouseover','.card-link-digital',function () {
+    crd = _cardsdigital.filter(c=>c.code==$(this).data("code"))[0]
+    container = $(this).parent();
+    if (! is_touch_device) {
+      $(this).popover({
+        trigger: 'hover',
+        placement: 'auto',
+        container: container,
+        html: true,
+        title: 
+          '<span class="h4 clearfix">' 
+          + (crd.is_unique ? icon_unique : '')
+          + crd.name 
+          + card_icon(crd)
+          + '</span>',
+        content: 					
+          // statbar
+          '<div>'
+            + (typeof crd.cost !== 'undefined' ? '<span class="mr-2 border rounded border-dark px-2">' + crd.cost + '</span>': '')
+            + '</span>'
+            + (typeof crd.threat !== 'undefined' ? '<span class="mr-2"><span class="lotr-type-threat mr-1"></span>' + crd.threat + '</span>' : '')
+            + (typeof crd.willpower !== 'undefined' ? '<span class="mr-2"><span class="lotr-type-willpower mr-1"></span>' + crd.willpower + '</span>' : '')
+            + (typeof crd.attack !== 'undefined' ? '<span class="mr-2"><span class="lotr-type-attack mr-1"></span>' + crd.attack + '</span>' : '')
+            + (typeof crd.defense !== 'undefined' ? '<span class="mr-2"><span class="lotr-type-defense mr-1"></span>' + crd.defense + '</span>' : '')
+            + (typeof crd.health !== 'undefined' ? '<span class="mr-2"><span class="fas fa-heart mr-1"></span>' + crd.health + '</span>' : '')
+          + '</div>'
+          + (typeof crd.traits !== 'undefined' ? '<div class="text-center mb-1"><b><em>' + crd.traits + '</em></b></div>' : '') 
+          + '<div class="wsprewrap">' + (typeof crd.text !== 'undefined" ? lortdb_markdown(crd.text) : '') + '</div>'
+          + '<small class="text-muted ml-auto">'+ crd.pack_name + ' #' + crd.position + '</small>'
+      }).popover('show');
+    }
+  });
