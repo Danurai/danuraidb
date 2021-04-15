37e033347f4675058f81d6b90cbad632bf30d89c
Danurai <Danurai@users.noreply.github.com>
2021-04-15 17:13:47 +0100

    Add files via upload

--- resources/public/js/whuw/whuw_yauwdbdata.js
+++ resources/public/js/whuw/whuw_yauwdbdata.js
@@ -13,67 +13,48 @@ $.get(url, data => {
     });
 
     $('#faction').on('change', function () {
-        let faction = this.value;
-        let factionId = _factions[ faction ].id;
-        let sets = new Set( Object.values(_cards).filter( c => c.factionId == factionId ).map( c => c.setId ) );
+        let faction = _factions[ this.value ];
         $('#faction-members').empty();
-        viewFighters( faction );
-        viewFactionCards( faction );
-        viewSetCards( sets );
+        viewFighters( [ faction ]);
+        viewFactionCards( faction, $('#display').val() );
+        syncSet( faction );
     });
+
+    function syncSet( faction ) {
+        let setId = Object.values( _cards ).filter( c => c.factionId == faction.id )[0].setId;
+        let set = Object.values( _sets ).filter( s => s.id == setId)[0];
+        $('#set').val(set.displayName);
+    }
 
     $('#set').on('change', function () {
         let set = _sets[ this.value ];
         let factionIds = set.displayName == "Leaders" || set.displayName == "Power Unbound"
             ? new Set()
             : new Set( Object.values( _cards ).filter( c => c.setId == set.id ).map( c => c.factionId ) );
-        let factions = Object.values( _factions ).filter( f => factionIds.has( f.id ) );
-        $('#faction-members').empty();
-        factions.forEach( f => viewFighters( f.displayName ) );
-        $('#faction-cards').empty();
-        viewSetCards( [ set.id ] );
+        let factionArray = Object.values( _factions ).filter( f => factionIds.has( f.id ) );
+        viewFighters( factionArray );
+        viewSetCards( set, $('#display').val()  );
     })
+    $('#display').on('change', function () {
+        $('#fs-toggle').hasClass('active')
+            ? $('#set').trigger('change')
+            : $('#faction').trigger( 'change' );
+    });
 
-    function viewFighters ( faction ) {
-        let factionName = _factions[ faction ].name;
-        let members = _factionMembers[ factionName ];
-        let factionFighters = $('<div class="whuw__faction-fighters d-flex flex-wrap justify-content-center mb-2"></div>');
-        $('#faction-members')
-            .append ( factionFighters );
-        if ( typeof members != 'undefined' ) {
-            members.forEach( ( m, i ) => {
-                factionFighters.append( factionMember( factionName, m, i + 1 ) );
-            })
-        }
-    }
-
-    function viewFactionCards( faction ) {
-        let factionId = _factions[ faction ].id;
-        let factionCards = $('<div class="whuw__faction-cards d-flex flex-wrap justify-content-center"></div>')
-        $('#faction-cards')
-            .empty()
-            .append(factionCards);
-        Object.values(_cards).filter( c => c.factionId == factionId ).forEach( c => {
-            factionCards.append( cardElement( c ) );
+    function viewFighters ( factionArray ) {
+        $('#faction-members').empty();
+        factionArray.forEach( f => {
+            let members = _factionMembers[ f.name ];
+            let factionFighters = $('<div class="whuw__faction-fighters d-flex flex-wrap justify-content-center mb-2"></div>');
+            if ( typeof members != 'undefined' ) {
+                $('#faction-members').append ( factionFighters );
+                members.forEach( ( m, i ) => {
+                    factionFighters.append( fighterCards( f.name, m, i + 1 ) );
+                })
+            }
         });
     }
-
-    function viewSetCards( sets ) {
-        $('#set-cards').empty();
-        sets.forEach( setId => {
-            let set = Object.values( _sets ).filter( s => s.id == setId )[0];
-            let setWrapper = $('<div></div>');
-            let cardsWrapper = $('<div class = "d-flex flex-wrap justify-content-center"></div>');
-            $('#set-cards').append( setWrapper );
-            setWrapper.append(`<div class="text-light mb-2 text-center"><b>${set.displayName}</b></div>`);
-            setWrapper.append( cardsWrapper );
-            Object.values( _cards ).filter( c => c.setId == setId ).forEach( c => {
-                cardsWrapper.append( cardElement( c, false) );
-            });
-        });
-    }
-
-    function factionMember( factionName, m, i ) {
+    function fighterCards( factionName, m, i ) {
         let srcBase = `/img/whuw/fighters/${factionName}-${i}.webp`;
         let srcInspired = `/img/whuw/fighters/${factionName}-${i}-inspired.webp`;
         let innerHTML = `<div class="whuw__fighter-cards">
@@ -85,7 +66,64 @@ $.get(url, data => {
                         </div>`
         return innerHTML; 
     }
-    
+
+
+    function viewSetCards( set, view ) {
+        viewCards( Object.values( _cards ).filter( c => c.setId == set.id ), set.displayName.match(/\w+$/)[0], view );
+    }
+    function viewFactionCards( faction, view ) {
+        viewCards( Object.values(_cards).filter( c => c.factionId == faction.id ), "Faction", view );
+    }
+
+    function viewCards( cards, filter, view ) {
+        let factionCards;
+        let header = `<h5 class="text-center text-capitalize">${filter} Cards</h5>`;
+        if ( view == 'List') {
+            let wrapper = $('<div class="container"></div>')
+            let factionCardTable = $('<table class="table text-light"></table>');
+            let factionCardThead = $('<thead><tr><th class="d-none">ID</th><th class="text-center">Faction</th><th class="text-center">Type</th><th>Name</th><th class="text-center">Glory</th><th class="text-center">Wave</th><th class="text-center">#</th></tr></thead>');
+            factionCards = $('<tbody></tbody>');
+            $('#card-list')
+                .empty()
+                .append( header )
+                .append( 
+                    wrapper.append( 
+                        factionCardTable
+                            .append( factionCardThead )
+                            .append( factionCards )));
+        } else {
+            factionCards = $('<div class="whuw__faction-cards d-flex flex-wrap justify-content-center"></div>');
+            $('#card-list').empty().append( header ).append( factionCards );
+        
+        }
+        cards.forEach( c => {
+            switch ( view ) {
+                case 'List':    
+                    factionCards.append( cardRow( c ) );
+                    break;
+                case 'Image':   
+                    factionCards.append( cardImgElement( c ) );
+                    break;
+                default:        
+                    factionCards.append( cardElement( c ) );
+            }
+        });
+    }
+
+    function cardRow( card ) {
+        let wave = String(Math.floor(card.id / 1000));
+        let set = Object.values(_sets).filter( s => s.id == card.setId )[0];
+        let faction = Object.values(_factions).filter( f => f.id == card.factionId)[0];
+        return `<tr>
+                <td class="d-none">${card.id}</td>
+                <td class="text-center"><img class="icon-sm" src="/img/whuw/icons/${faction.name}-icon.png" alt="${faction.name}" title="${faction.displayName}"></img></td>
+                <td class="text-center"><img class="icon-sm" src="/img/whuw/icons/type_${card.type.toLowerCase()}.png" alt="${card.type}" title="${card.type}"></img></td>
+                <td><span style="cursor: pointer;" data-toggle="modal" data-target="#card-modal" data-cardid="${card.id}">${card.name}</a></td>
+                <td class="text-center">${ card.glory != null ? card.glory : ''}</td>
+                <td class="text-center"><img class="icon-sm" src="/img/whuw/icons/wave-${wave.padStart( 2, '0')}-icon.png" title="Wave ${wave}"></td>
+                <td class="text-center">#${card.id % 1000}/${_waveCounts[ wave ]}</td>
+            </tr>`;
+    }
     function cardElement( card, nameSet = true ) {
         let wave = String(Math.floor(card.id / 1000));
         let set = Object.values(_sets).filter( s => s.id == card.setId )[0];
@@ -119,7 +157,6 @@ $.get(url, data => {
                 </div>
             </div>`
     }
-
     function warhammerUnderworldsCardURL( card ) {
         let setPrefix = [ '-', 'S', 'L', 'NV', 'NVPU', 'COD', 'BG', 'BGGIFTPACK', 'ARENAMORTIS', 'DIRECHASM' ];
         let cycle = Math.round( card.id / 1000 );
@@ -146,9 +183,6 @@ $.get(url, data => {
         });
     
     $('#card-modal').on('show.bs.modal', function (evt) {
-        //let card = _cards[ $(evt.relatedTarget).data('card-id') ];
-        //let imgname = String(card.id).padStart(5, '0');
-        //let src = `/img/whuw/assets/cards/${imgname}`;
         let src = evt.relatedTarget.src;
         if (typeof src == 'undefined') {
             $('#card-modal').find('.modal-body').html(
@@ -161,21 +195,19 @@ $.get(url, data => {
         }
     });
 
-    $('#filteroptions').on('change', function () {
-        let opt = $(this).find('input:checked')[0].id;
-        if ( opt == 'opt-faction' ) {
-            $('#faction').css('display', 'block');
-            $('#set').css('display', 'none');
+    $('#fs-toggle').on('click', function() {
+        $('#factionset select').toggleClass('d-none');
+        if ( $(this).hasClass('active') ) {
+            $('#fs-label').text("Faction");
             $('#faction').trigger('change');
         } else {
-            $('#faction').css('display', 'none');
-            $('#set').css('display', 'block');
+            $('#fs-label').text("Set");
             $('#set').trigger('change');
-        }
-        console.log( opt );
-    });
+        };
+    })
 
-    $('#faction')[0].value = `Khagra's Ravagers`;
+    $('#set')[0].value = $('#set option').last().val();
+    $('#faction')[0].value = $('#faction option').last().val();
     $('#faction').trigger('change');
 });
 
@@ -183,11 +215,15 @@ function whuw_markdown( str ) {
     /*
         **...** bold
         \n newline
-        [...]
+        [...] Attack Action
+        :xxx: Symbol
     */
     return str
             .replace(/\*{2}(.*?)\*{2}/g, '<b>$1</b>')
             .replace(/\*(.*?)\*/g, '<em>$1</em>')
             .replace(/\\n/g, '<br />')
-            .replace(/\[(.+)\]/g,'<div class="p-2 text-white rounded" style="background-color: black">$1</div>');
+            .replace(/\:(\w+)\:/g, '<i class="icon-$1"></i>')
+            .replace(/\-\((.*?)\)\-/g, '<div class="action-effect">$1</div>')
+            .replace(/\s(\-|[0-9])\s\-/g, ' $1 ')
+            .replace(/\[(.*?)\]/g,'<div class="h5 rounded action">$1</div><br />');
 }
